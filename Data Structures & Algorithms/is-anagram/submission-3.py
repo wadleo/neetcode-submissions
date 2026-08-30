849b1699ba00d7baa4dad960c74ee6db0638eabb
Wadinga Leonard <wadingaleonard@gmail.com>
2026-08-30 07:54:19 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        for x in s:
+            xc = s.count(x)
+            if x not in t or t.count(x) != xc:
+                return False
+        # for x in t:
+        #     xc = t.count(x)
+        #     if x not in s or s.count(x) != xc:
+        #         return False
+        return True
